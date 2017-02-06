5f6085c896d9d90a4bab47dbf46fb629ca6a41ee
jimtonic <ferdi@glasspod.de>
2017-02-06 11:53:20 +0100

    fixed test for KairosDBClient::insert

--- kairosdb/src/test/java/com/yahoo/ycsb/db/TestKairosDBClient.java
+++ kairosdb/src/test/java/com/yahoo/ycsb/db/TestKairosDBClient.java
@@ -1,19 +1,20 @@
 package com.yahoo.ycsb.db;
 
+import com.yahoo.ycsb.ByteIterator;
 import com.yahoo.ycsb.Status;
 import com.yahoo.ycsb.StringByteIterator;
+import com.yahoo.ycsb.generator.UniformDoubleGenerator;
+import com.yahoo.ycsb.generator.UnixEpochTimestampGenerator;
 import com.yahoo.ycsb.measurements.Measurements;
 import com.yahoo.ycsb.workloads.WinnerWorkload;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
+import org.junit.*;
 import org.kairosdb.client.HttpClient;
 import org.kairosdb.client.builder.AggregatorFactory;
 import org.kairosdb.client.builder.QueryBuilder;
 import org.kairosdb.client.builder.TimeUnit;
 import org.kairosdb.client.response.QueryResponse;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
@@ -31,6 +32,9 @@ public class TestKairosDBClient {
 
   private KairosDBClient client;
   private HttpClient httpClient;
+
+  private UnixEpochTimestampGenerator tGen;
+  private UniformDoubleGenerator vGen;
 
   @Before
   public void setUp() throws Exception {
@@ -51,6 +55,8 @@ public class TestKairosDBClient {
 
     httpClient = new HttpClient(String.format("http://%s:%s", HOST, PORT));
 
+    tGen = new UnixEpochTimestampGenerator(1, java.util.concurrent.TimeUnit.SECONDS, 1);
+    vGen = new UniformDoubleGenerator(-10.0, 10.0);
   }
 
   @After
@@ -64,30 +70,21 @@ public class TestKairosDBClient {
 
   @Test
   @Ignore
-  public void testInsertStaticDatapoint() throws Exception {
-    final HashMap<String, String> tags = new HashMap<String, String>();
-    tags.put("tag0", "value1");
-    tags.put("tag1", "value2");
-
-    Long currentTimestamp = System.currentTimeMillis() - new Long("2678400000");
-    Double value = 100.0;
-
-    String key = METRIC + ":" + currentTimestamp + ":" + value;
-
-    Status status = client.insert(TABLE, key, StringByteIterator.getByteIteratorMap(tags));
-    assertEquals(Status.OK, status);
+  public void testInsert() throws Exception {
+    for (int i = 0; i < 1000; i++) {
+      Status status = client.insert("samples", getNextKey(), getTags());
+      Assert.assertEquals(Status.OK, status);
+    }
 
     java.util.concurrent.TimeUnit.SECONDS.sleep(1);
 
     QueryBuilder builder = QueryBuilder.getInstance();
-    builder.setStart(2, TimeUnit.MONTHS)
+    builder.setStart(new Date(1))
         .addMetric(METRIC)
-        .addAggregator(AggregatorFactory.createAverageAggregator(10, TimeUnit.MONTHS));
-
+        .addAggregator(AggregatorFactory.createCountAggregator(100, TimeUnit.YEARS));
     QueryResponse response = httpClient.query(builder);
-
     assertEquals(response.getStatusCode(), 200);
-    assertThat(response.getBody(), containsString("\"sample_size\":1"));
+    assertThat(response.getBody(), containsString("\"sample_size\":1000"));
   }
 
   @Test
@@ -99,5 +96,20 @@ public class TestKairosDBClient {
     for (Map.Entry<String,String> entry : tagpool.entrySet()) {
       assertEquals(10, entry.getValue().length());
     }
+  }
+
+  private String getNextKey() {
+    long ts = tGen.nextValue();
+    double val = vGen.nextValue();
+    String key = "sensormetric:"+ts+":"+val;
+    return key;
+  }
+
+  private HashMap<String, ByteIterator> getTags() {
+    HashMap<String, String> tags = new HashMap<>();
+    for (int i = 0; i < 5; i++) {
+      tags.put("tag"+i, "value"+i);
+    }
+    return StringByteIterator.getByteIteratorMap(tags);
   }
 }
